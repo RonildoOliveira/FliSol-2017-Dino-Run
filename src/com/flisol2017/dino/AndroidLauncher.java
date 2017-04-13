9715ceb823ab4e6418d0142826896ff25eac913d
RonildoOliveira <ronildo.comp@gmail.com>
2017-04-13 15:38:23 -0300

    Dino Run FliSol 2017

---
+++ src/com/flisol2017/dino/AndroidLauncher.java
@@ -0,0 +1,15 @@
+package com.flisol2017.dino;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameRun(), config);
+	}
+}
